eb3c995059aa2745ceda53ff4e85fa193aa0fc52
unknown <maayanlevi2001@gmail.com>
2024-11-14 20:18:59 +0200

    Fix Ascend

--- Ascend.java
+++ Ascend.java
@@ -2,12 +2,13 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
+		// get input of range limit
 		int lim =  Integer.parseInt(args[0]);
-
+        // creating 3 random nums inside the range
 		int a = (int) (lim * Math.random());
 		int b = (int) (lim * Math.random());
 		int c = (int) (lim * Math.random());
-        // print random numbers
+        // print the random numbers
 		System.out.println(a + " " + b + " " + c);
 		//Ascending order
 		int min = Math.min(a, Math.min(b, c));
